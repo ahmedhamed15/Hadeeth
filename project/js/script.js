87bdf370203b348ff51be2d3a6ad11fca98eda1b
Ahmed Hamed <ahmed.hamed@ihorizons.com>
2020-10-31 17:01:14 +0200

    Add close icon for Maqased sidebar in the Mobile

--- project/js/script.js
+++ project/js/script.js
@@ -5,8 +5,14 @@ $(function() {
     var windowVar = $(window);
     /* End :: All Variables */
 
+    /* Start : Add close icon for Maqased sidebar */
+    if (windowVar.width() < 769) {
+        $('.sidebar-maqased').append('<span class="close-maqased-sidebar"> <i class="fas fa-times"></i> </span>');
+    }
+    /* End : Add close icon for Maqased sidebar */
+
     /* Start : Sidebar */
-    $('#sidebarCollapse-maqased').on('click', function () {
+    $('#sidebarCollapse-maqased, .close-maqased-sidebar').on('click', function () {
         $('.sidebar-maqased, #content').toggleClass('active');
         $('.collapse.in').toggleClass('in');
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
@@ -29,6 +35,9 @@ $(function() {
     $('.toggle-search-section').on('click', function() {
         $('.search-form').toggleClass('active');
     }); 
+    /* End : Toggle search box in mobile */
+
+    /* Start : Pie progress plugin In homepage */
     $('.pie_progress').asPieProgress({
         namespace: 'pie_progress',
         classes: {
@@ -56,51 +65,46 @@ $(function() {
         },
 
         contentCallback: null
-      });
-      $('.pie_progress').asPieProgress('start');
+    });
+    $('.pie_progress').asPieProgress('start');
+    /* End : Pie progress plugin In homepage */
 
-       // Copy function
-        function copyMessage(val) {
+    /* Start : Copy function */
+    function copyMessage(val) {
+        const selBox = document.createElement('textarea');
+        selBox.style.position = 'fixed';
+        selBox.style.left = '0';
+        selBox.style.top = '0';
+        selBox.style.opacity = '0';
+        selBox.value = val;
+        document.body.appendChild(selBox);
+        selBox.focus();
+        selBox.select();
+        document.execCommand('copy');
+        document.body.removeChild(selBox);
+    }
+    /* End : Copy function */
 
-            // Copy text
-            const selBox = document.createElement('textarea');
-            selBox.style.position = 'fixed';
-            selBox.style.left = '0';
-            selBox.style.top = '0';
-            selBox.style.opacity = '0';
-            selBox.value = val;
-            document.body.appendChild(selBox);
-            selBox.focus();
-            selBox.select();
-            document.execCommand('copy');
-            document.body.removeChild(selBox);
+    /* Start : Copy for Hadeeth text */
+    $('.copyHadeeth').on('click', function() {
+        copyMessage($('.hadeeth-body').text());
+        swal("تم النسخ", "تم نسخ الحديث بنجاح", "success");
+    });
+    /* End : Copy for Hadeeth text */
 
-            
-
+    /* Start : Show and hide the password */
+    $('.show-hide-password').on('click', function() {
+        var passField = $('.main-form .password-field'),
+            $this = $(this);
+        if (passField.prop('type') == "password") {
+            passField.prop('type', 'text');
+            $this.find('svg.fa-lock').addClass('active').siblings().removeClass('active');
+        } else {
+            passField.prop('type', 'password');
+            $this.find('svg.fa-lock').removeClass('active').siblings().addClass('active');
         }
-
-        // Hadeeth copy
-        $('.copyHadeeth').on('click', function() {
-            copyMessage($('.hadeeth-body').text());
-            swal("تم النسخ", "تم نسخ الحديث بنجاح", "success");
-        });
-
-        // $('.copy-explain-hadeeth').on('click', function() {
-        //     console.log('Ahmed hamed')
-        //     copyMessage($('.explain-hadeeth').text());
-        //     swal("تم النسخ", "تم نسخ شرح الحديث بنجاح", "success");
-        // });
-
-        $('.show-hide-password').on('click', function() {
-            var passField = $('.main-form .password-field'),
-                $this = $(this);
-            if (passField.prop('type') == "password") {
-                passField.prop('type', 'text');
-                $this.find('svg.fa-lock').addClass('active').siblings().removeClass('active');
-            } else {
-                passField.prop('type', 'password');
-                $this.find('svg.fa-lock').removeClass('active').siblings().addClass('active');
-            }
-        })
+    })
+    /* End : Show and hide the password */
+        
 
 });
